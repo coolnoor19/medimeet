87fabd6dd93af9d19948268066c17f30d1d433d3
Noor Alam <nooralam192001@gmail.com>
2025-06-25 23:44:14 +0530

    feat/Card: add Card section in the homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,14 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { features } from "@/lib/data";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -54,6 +63,39 @@ export default function Home() {
                 className="object-cover "
               />
             </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Card Section */}
+      <section className="py-20 bg-muted/30">
+        <div className="container mx-auto px-10">
+          <div className="text-center mb-16">
+            <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">
+              How It Works
+            </h2>
+            <p className="text-muted-foreground text-lg max-w-2xl mx-auto">
+              {" "}
+              Our platform makes healthcare accessible with just a few clicks
+            </p>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {features.map((feature, index) => (
+              <Card
+                key={index}
+                className="bg-card border-emerald-900/20 hover:border-emerald-800/40 transition-all duration-300"
+              >
+                <CardHeader className="pb-2">
+                  <div className="bg-emerald-900/20 p-3 rounded-lg w-fit mb-4">
+                    {feature.icon}
+                  </div>
+                  <CardTitle className="text-xl font-semibold text-white">{feature.title}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p  className="text-muted-foreground">{feature.description}</p>
+                </CardContent>
+              </Card>
+            ))}
           </div>
         </div>
       </section>
